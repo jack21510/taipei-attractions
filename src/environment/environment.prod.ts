c8402a1dda3485cfe4845c42fbf66dfa039848c2
jackchen <jack21510@gmail.com>
2025-09-01 20:51:41 +0800

    feat:新增環境變數

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  IS_PRINT_CONSOLE_LOG: false,
+  appVersion: require('package.json').version + '-prod',
+  production: true,
+  /** Prod */
+  API_DEFAULT_IP: 'https://www.travel.taipei/open-api',
+  PAGE_SIZE: 10,
+
+};
